8a1252aee6eff533dc43cec2103424d814e82f18
WescleyHenrique <wescleyhenrique0@gmail.com>
2025-03-20 21:54:07 +0000

    definido as variaveis

--- xadrez.c
+++ xadrez.c
@@ -3,16 +3,27 @@
 // Desafio de Xadrez - MateCheck
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
-
-int main() {
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
+    #define TORRE_MOVIMENTO 5
+    #define BISPO_MOVIMENTO 5
+    #define RAINHA_MOVIMENTO 8
 
-    // Implementação de Movimentação do Bispo
+    
+int main() {
+
+    // Implementação de Movimentação da torre
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+    printf("MOVIMENTAÇÃO DA TORRE:\n");
 
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
+    for (int i = 1;i <= TORRE_MOVIMENTO; i++) 
+    {
+        printf("MOVENDO A TORRE EM LINHA RETA %d CASA(S)\n", i);
+    }
+    
+
+    // Implementação de Movimentação do bispo
+    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.    
 
     // Implementação de Movimentação da Rainha
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
